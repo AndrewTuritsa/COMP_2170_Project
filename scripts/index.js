8090d2dcb230af6e9132fe6e9eebf0c42b077194
juliadeng123 <jdeng43@my.bcit.ca>
2024-05-09 22:18:44 -0700

    add framework

---
+++ scripts/index.js
@@ -0,0 +1,114 @@
+let boardWidth = 1000;
+let boardHeight = 600;
+let context;
+
+let crabWidth = 90;
+let crabHeight = 150;
+let crabX = 50;
+let crabY = boardHeight-crabHeight;
+// let crabImg;
+
+let crab = {
+    x : crabX,
+    y: crabY,
+    width : crabWidth,
+    height : crabHeight
+}
+
+//obstacle - plastic, bottles, etc
+
+let obstacleArray = [];
+
+let obstacle1Width = 130;
+let obstacle2Width = 150;
+let obstacle3Width = 160;
+
+let obstacleHeight = 90;
+let obstacleX = 700;
+let obstacleY = boardHeight - obstacleHeight;
+
+// let obstacle1Img;
+// let obstacle2Img;
+// let obstacle3Img;
+
+//physics/speed
+let velocityX = -10;
+let velocityY = 0;
+let gravity = .4;
+
+let score = 0;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d");
+
+
+    //crabbox
+    context.fillStyle ="red";
+    context.fillRect(crab.x, crab.y, crab.width, crab.height)
+    
+    //crabimage
+    // crabImg = new Image();
+    // crabImg.src = "./img/...";
+    // crabImg.onload = function() {
+    //     context.drawImage(crabImg,crab.x, crab.y, crab.width, crab.height);
+    // }
+    // obstacle1Img = new Image();
+    // obstacle1Img.src = "./img/obstacle1.src";
+
+    // obstacle2Img = new Image();
+    // obstacle2Img.src = "./img/obstacle2.src";
+
+    // obstacle3Img = new Image();
+    // obstacle3Img.src = "./img/obstacle3.src";
+    
+    requestAnimationFrame(update);
+    setInterval(placeObstacle, 1000);
+}
+
+function update() {
+    requestAnimationFrame(update);
+
+    //context.drawImage(crabImg,crab.x, crab.y, crab.width, crab.height);
+    context.clearRect(0, 0, board.width, board.height)
+    context.fillStyle ="red";
+    context.fillRect(crab.x, crab.y, crab.width, crab.height)
+    
+    for (let i = 0; i < obstacleArray.length; i++) {
+        let obstacle = obstacleArray[i];
+        obstacle.x += velocityX;
+        context.fillStyle ="white";
+        context.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height)
+    }
+}
+
+function placeObstacle() {
+    
+    //place obstacle
+    let obstacle = {
+        x : obstacleX,
+        y : obstacleY,
+        width : null,
+        height : obstacleHeight
+    }
+
+    let placeObstacleChance = Math.random();
+
+    if (placeObstacleChance > 0.9) {
+        obstacle.width = obstacle3Width;
+        obstacleArray.push(obstacle);
+    }
+
+    else if (placeObstacleChance > 0.7) {
+        obstacle.width = obstacle2Width;
+        obstacleArray.push(obstacle);
+    }
+    else if (placeObstacleChance > 0.5) {
+        obstacle.width = obstacle1Width;
+        obstacleArray.push(obstacle);
+    }
+
+}
